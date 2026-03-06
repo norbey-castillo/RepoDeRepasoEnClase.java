ae1a21838a349a38a9c0a5b9bced7abe5587e7c8
Norbey <norbeycastillo890@gmail.com>
2026-03-05 18:58:06 -0600

    Ejercicio No. 2

--- Repaso/src/Repaso/Ejercicios.java
+++ Repaso/src/Repaso/Ejercicios.java
@@ -27,9 +27,34 @@ public class Ejercicios {
 		}
 	}
 
+					
+					//	PROGRAMA 2
+	public static int factorial(int numero) {
+		if(numero == 0) {
+			System.out.print("El numero 0 no tiene factorial. ");
+			return -1;
+		}
+		if(numero == 1 || numero == 2) {
+			return 1;
+		}
+		int resultado = 1;
+		for(int i = 2; i <= numero; i++) {
+			resultado = resultado * i;
+		}
+		
+		return resultado;
+	}
+	public static void Programa2() {
+		System.out.print("Ingrese un numero para ver su factorial: ");
+		int numero = sc.nextInt();
+		
+		int Factorial = factorial(numero);
+		
+		System.out.println("El factorial de: "+numero+" Es: "+Factorial);
+	}
+	
 	
 	public static void main(String[] args) {
-		
 
 	}
 
